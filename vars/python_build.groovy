4aaab6510a821252f8206243b940fc7ca461b960
Jovan9876 <78658024+Jovan9876@users.noreply.github.com>
2022-11-21 16:26:05 -0800

    Update python_build.groovy

--- vars/python_build.groovy
+++ vars/python_build.groovy
@@ -1,9 +1,10 @@
 def call() {
-  pipeline {
-      agent any
+    pipeline {
+    agent any
+    stages {
         stage('Build') {
             steps {
-                echo 'Building...'
+                echo 'Building..'
             }
         }
         stage('Python Lint'){
@@ -11,16 +12,16 @@ def call() {
                 sh 'pylint-fail-under --fail_under 5.0 **.py'
             }
         }
-          stage('Test') {
-              steps {
-                  echo 'Testing..'
-              }
-          }
-          stage('Deploy') {
-              steps {
-                  echo 'Deploying....'
-              }
-          }
-      }
+        stage('Test') {
+            steps {
+                echo 'Testing..'
+            }
+        }
+        stage('Deploy') {
+            steps {
+                echo 'Deploying....'
+            }
+        }
+    }
   }
-
+}
