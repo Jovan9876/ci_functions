3f06ad6c119c68ac1e1a9d6f39df6515ee6a4f92
Jovan9876 <78658024+Jovan9876@users.noreply.github.com>
2022-11-21 16:16:18 -0800

    Update python_build.groovy

--- vars/python_build.groovy
+++ vars/python_build.groovy
@@ -1,12 +1,16 @@
 def call() {
   pipeline {
       agent any
-      stages {
-          stage('Build') {
-              steps {
-                  echo 'Building..'
-              }
-          }
+        stage('Build') {
+            steps {
+                sh 'pip install -r requirements.txt'
+            }
+        }
+        stage('Python Lint'){
+            steps {
+                sh 'pylint-fail-under --fail_under 5.0 **.py'
+            }
+        }
           stage('Test') {
               steps {
                   echo 'Testing..'
