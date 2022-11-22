2a4b2b6c266e6508d48844d43010631e02a12697
Jovan9876 <78658024+Jovan9876@users.noreply.github.com>
2022-11-21 16:31:08 -0800

    Update python_build.groovy

--- vars/python_build.groovy
+++ vars/python_build.groovy
@@ -4,7 +4,7 @@ def call() {
     stages {
         stage('Build') {
             steps {
-                echo 'Building..'
+                sh 'pip install -r requirements.txt'
             }
         }
         stage('Python Lint'){
