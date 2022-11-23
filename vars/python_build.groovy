92cc47b756962a749a18d13145f9a3052c8ddc71
Jovan9876 <78658024+Jovan9876@users.noreply.github.com>
2022-11-23 13:00:11 -0800

    Update python_build.groovy

--- vars/python_build.groovy
+++ vars/python_build.groovy
@@ -22,14 +22,14 @@ def call() {
             steps {
                 withCredentials([string(credentialsId: 'DockerHub', variable: 'TOKEN')]) {
                     sh "docker login -u 'jovan9876' -p '$TOKEN' docker.io"
-                    sh "docker build -t reciever:latest -f reciever.Dockerfile --tag jovan9876/reciever:reciever ."
-                    sh "docker push jovan9876/reciever:reciever"
-                    sh "docker build -t storage:latest -f storage.Dockerfile --tag jovan9876/storage:storage ."
-                    sh "docker push jovan9876/storage:storage"
-                    sh "docker build -t processing:latest -f processing.Dockerfile --tag jovan9876/processing:processing ."
-                    sh "docker push jovan9876/processing:processing"
-                    sh "docker build -t audit_log:latest -f audit_log.Dockerfile --tag jovan9876/audit_log:audit_log ."
-                    sh "docker push jovan9876/audit_log:audit_log"
+                    sh "docker build -t reciever:latest -f reciever.Dockerfile --tag jovan9876/reciever:latest ."
+                    sh "docker push jovan9876/reciever:latest"
+                    sh "docker build -t storage:latest -f storage.Dockerfile --tag jovan9876/storage:latest ."
+                    sh "docker push jovan9876/storage:latest"
+                    sh "docker build -t processing:latest -f processing.Dockerfile --tag jovan9876/processing:latest ."
+                    sh "docker push jovan9876/processing:latest"
+                    sh "docker build -t audit_log:latest -f audit_log.Dockerfile --tag jovan9876/audit_log:latest ."
+                    sh "docker push jovan9876/audit_log:latest"
                 }
             }
         }
@@ -49,26 +49,9 @@ def call() {
                 } 
             }
         }
-        def remote = [:]
-        remote.name = "ACIT3855"
-        remote.host = "acit-3855.eastus.cloudapp.azure.com"
-        remote.allowAnyHosts = true
-        remote.user = 'azureuser'
         stage('Deploy') {
             steps {
                 echo 'Deploying....'
-                    withCredentials([sshUserPrivateKey(credentialsId: 'sshUser', keyFileVariable: 'identity', passphraseVariable: '')]) {
-
-                    remote.identityFile = identity
-                    stage("SSH Steps Rocks!") {
-                        writeFile file: 'abc.sh', text: 'ls'
-                        sshCommand remote: remote, command: 'for i in {1..5}; do echo -n \"Loop \$i \"; date ; sleep 1; done'
-                        sshPut remote: remote, from: 'abc.sh', into: '.'
-                        sshGet remote: remote, from: 'abc.sh', into: 'bac.sh', override: true
-                        sshScript remote: remote, script: 'abc.sh'
-                        sshRemove remote: remote, path: 'abc.sh'
-                    }
-            }
         }
     }
   }
