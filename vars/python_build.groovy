481e858dacf65e4a5bc8850704a8147a0a42728d
Jovan9876 <78658024+Jovan9876@users.noreply.github.com>
2022-11-23 11:41:42 -0800

    Update python_build.groovy

--- vars/python_build.groovy
+++ vars/python_build.groovy
@@ -49,11 +49,25 @@ def call() {
                 } 
             }
         }
-
+        def remote = [:]
+        remote.name = "ACIT3855"
+        remote.host = "acit-3855.eastus.cloudapp.azure.com"
+        remote.allowAnyHosts = true
+        remote.user = 'azureuser'
         stage('Deploy') {
             steps {
                 echo 'Deploying....'
+                    withCredentials([sshUserPrivateKey(credentialsId: 'sshUser', keyFileVariable: 'identity', passphraseVariable: '')]) {
 
+                    remote.identityFile = identity
+                    stage("SSH Steps Rocks!") {
+                        writeFile file: 'abc.sh', text: 'ls'
+                        sshCommand remote: remote, command: 'for i in {1..5}; do echo -n \"Loop \$i \"; date ; sleep 1; done'
+                        sshPut remote: remote, from: 'abc.sh', into: '.'
+                        sshGet remote: remote, from: 'abc.sh', into: 'bac.sh', override: true
+                        sshScript remote: remote, script: 'abc.sh'
+                        sshRemove remote: remote, path: 'abc.sh'
+                    }
             }
         }
     }
