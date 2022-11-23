89c19693caa8492b58f6a650f38002eb1795a6ea
Jovan9876 <78658024+Jovan9876@users.noreply.github.com>
2022-11-22 23:57:13 -0800

    Update python_build.groovy

--- vars/python_build.groovy
+++ vars/python_build.groovy
@@ -24,11 +24,11 @@ def call() {
                     sh "docker login -u '<username>' -p '$TOKEN' docker.io"
                     sh "docker build -t reciever:latest -f reciever/ --tag reciever/reciever:reciever ."
                     sh "docker push jovan9876/reciever:reciever"
-                    sh "docker build -t storage:latest -f storage/ --tag storage/storage:storage .
+                    sh "docker build -t storage:latest -f storage/ --tag storage/storage:storage ."
                     sh "docker push jovan9876/storage:storage"
-                    sh "docker build -t processing:latest -f processing/ --tag processing/processing:processing .
+                    sh "docker build -t processing:latest -f processing/ --tag processing/processing:processing ."
                     sh "docker push jovan9876/processing:processing"
-                    sh "docker build -t audit_log:latest -f audit_log/ --tag audit_log/audit_log:audit_log .
+                    sh "docker build -t audit_log:latest -f audit_log/ --tag audit_log/audit_log:audit_log ."
                     sh "docker push jovan9876/audit_log:audit_log"
                 }
             }
